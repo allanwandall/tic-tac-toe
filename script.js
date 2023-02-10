e55197bcf260855d7e33137ccf134d41e8a93247
Allan Wandall <allanwandall@gmail.com>
2023-02-10 01:46:37 +0100

    Create functional prototype
    functionality and interaction has been added in javascript and the game is functioning well. At the moment the computer simply chooses random fields. A notification on gameend is still missing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-
+//The gameBoard module had the main function of managing an array which represents the game fields
 const gameBoard = (() => {
     let _board = new Array(9);
 
@@ -9,8 +9,8 @@ const gameBoard = (() => {
     }
 
     const setField = (player, idx) => {
-        const domField = document.querySelector(`.game-grid:nth-child(${idx + 1})`);
-        player.textContent = player.getSign();
+        const domField = document.querySelector(`.game-grid div:nth-child(${idx + 1})`);
+        domField.textContent = player.getSign();
         _board[idx] = player.getSign();
     }
 
@@ -44,13 +44,13 @@ const Player = (sign) => {
 } 
 
 const gameController = (() => {
-    const _humanPlayer = Player("X");
-    const _computerPlayer = Player("O");
+    const _humanPlayer = Player("");
+    const _computerPlayer = Player("");
 
     //checks row win conditions
     const _checkRows = (board) => {
         for (let i = 0; i <= 6; i += 3){
-            if (board[i] == board[i + 1] && board[i] == board[i + 2]) {
+            if (board.getField(i) == board.getField(i + 1) && board.getField(i) == board.getField(i + 2) && board.getField(i) !== undefined) {
                 return true;
             }
         }
@@ -60,7 +60,7 @@ const gameController = (() => {
     //check column win conditions
     const _checkColumns = (board) => {
         for (let i = 0; i <= 2; i++){
-            if (board[i] == board[i + 3] && board[i] == board[i + 6]) {
+            if (board.getField(i) == board.getField(i + 3) && board.getField(i) == board.getField(i + 6) && board.getField(i) !== undefined) {
                 return true;
             }
         }
@@ -69,9 +69,9 @@ const gameController = (() => {
 
     //check diagonal win conditions
     const _checkDiagonals = (board) => {
-        if (board[0] == board[4] && board[0] == board[8]) {
+        if (board.getField(0) == board.getField(4) && board.getField(0) == board.getField(8) && board.getField(0) !== undefined) {
             return true;
-        } else if (board[2] == board[4] && board[2] == board[6]) {
+        } else if (board.getField(2) == board.getField(4) && board.getField(2) == board.getField(6) && board.getField(2) !== undefined) {
             return true;
         }
         return false;
@@ -87,8 +87,8 @@ const gameController = (() => {
 
     //checks is the board is full and there is a draw
     const _checkDraw = (board) => {
-        for (let i = 0; i < board.length; i++){
-            if (board[i] == undefined) {
+        for (let i = 0; i < 9; i++){
+            if (board.getField(i) == undefined) {
                 return false;
             }
         }
@@ -97,39 +97,131 @@ const gameController = (() => {
 
     // changes the sign of player object
     const changeSign = (sign) => {
-        if (sign === "O") {
-            _humanPlayer.setSign = "O";
-            _computerPlayer.setSign = "X";
-        } else if (sign === "X") {
-            _humanPlayer.setSign = "X";
-            _computerPlayer.setSign = "O";
+        if (sign == "O") {
+            console.log("OH");
+            _humanPlayer.setSign("O");
+            _computerPlayer.setSign("X");
+        } else if (sign == "X") {
+            console.log("EKS");
+            _humanPlayer.setSign("X");
+            _computerPlayer.setSign("O");
+
+            //O moves first
+            _computerDelay(1000, 2000);
+
         } else throw 'Invalid sign';
     }
 
+    //Evaluates if the chosen field is available and then checks win conditions.
     const playerMove = (idx) => {
         const field = gameBoard.getField(idx);
 
         if (field == undefined) {
             gameBoard.setField(_humanPlayer, idx);
+            
             if (_checkWin(gameBoard)) {
                 _endGame(_humanPlayer.getSign());
+            } else if (_checkDraw(gameBoard)) {
+                _endGame("Draw");
+            } else {
+                _computerDelay(1000, 3000);
             }
-        }
+        } else {
+            console.log("filled");
+        }       
     }
 
+    const _endGame = (sign) => {
+        console.log(`Game ended. Result is: ${sign}`);
+        if (sign == _humanPlayer.getSign()) {
+            displayController.endGame("Player");
+        } else if (sign == _computerPlayer.getSign()) {
+            displayController.endGame("Computer");
+        } else {
+            displayController.endGame();
+        }
+        
+    }
+
+    const _sleep = (delayInms) => {
+        return new Promise(resolve => setTimeout(resolve, delayInms));
+    }
+
+    //Delay function for further development
+    const _computerDelay = (min, max) => {
+        displayController.deactivate();
+        displayController.changeTurn();
+        (async () => {
+
+            // Delay in milliseconds - a random number between min and max
+            const _delay = Math.floor(min + (Math.random() * (max - min)));
+            console.log(_delay);
+            await _sleep(_delay);
+            _computerMove(gameBoard);
+        })();
+        
+    }
+
+    //Computer chooses a random available field 
+    const _computerMove = () => {
+        let invalidField = true;
+        let rdmField;
+
+        while (invalidField) {
+            rdmField = Math.floor(Math.random() * 9);
+            if (gameBoard.getField(rdmField) == undefined) {
+                invalidField = false;
+            }
+        }
+        
+        gameBoard.setField(_computerPlayer, rdmField);
+        displayController.activate();
+
+        if (_checkWin(gameBoard)) {
+            _endGame(_computerPlayer.getSign());
+        } else if (_checkDraw(gameBoard)) {
+            _endGame("Draw");
+        }
+
+        displayController.changeTurn();
+    }
+
+    return {
+        changeSign,
+        playerMove
+    }
 
 })();
 
 const displayController = (() => {
 
     let _round = 1;
+    let _playerWins = 0;
+    let _computerWins = 0;
 
+    const _allHtmlFields = Array.from(document.querySelectorAll(".grid-square"));
+    const signContainer = document.querySelector(".sign-container");
+    const gameContainer = document.querySelector(".game-container");
+    const returnBtn = document.getElementById("return-button");
+    const oBtn = document.getElementById("o-button");
+    const xBtn = document.getElementById("x-button");
+    const playerTurn = document.getElementById("player-turn");
+    const computerTurn = document.getElementById("computer-turn");
+
+    //adds one to the round number and updates the display
     const _changeRound = () => {
         const roundTitle = document.querySelector(".round");
         _round++;
-        roundTitle.textContent = `Round: ${_round}`;
+        roundTitle.textContent = `Round ${_round}`;
     }
 
+    const _resetRound = () => {
+        _round = 1;
+        const roundTitle = document.querySelector(".round");
+        roundTitle.textContent = "Round 1";
+    }
+
+    //toggles between the game and sign container showing on the screen
     const _toggleWindows = () => {
         console.log("entered toggleWindows");
         if (!signContainer.classList.contains("hide")) {
@@ -146,10 +238,87 @@ const displayController = (() => {
         }
     }
 
-    const signContainer = document.querySelector(".sign-container");
-    const gameContainer = document.querySelector(".game-container");
-    const returnBtn = document.getElementById("return-button");
-    const oBtn = document.getElementById("o-button");
-    oBtn.addEventListener("click", _toggleWindows, false);
-    returnBtn.addEventListener("click", _toggleWindows, false);
+    const deactivate = () => {
+        _allHtmlFields.forEach(field => {
+            field.classList.add("disabled");
+        });
+    }
+
+    const activate = () => {
+        _allHtmlFields.forEach(field => {
+            field.classList.remove("disabled");
+        })
+    }
+
+    //add event listeners all the fields and buttons so they can be pressed to trigger a response
+    const _initFields = (() => {
+
+        for (let i = 0; i < _allHtmlFields.length; i++) {
+            _allHtmlFields[i].addEventListener("click", () => {
+                gameController.playerMove(i);
+            });
+        }
+
+        oBtn.addEventListener("click", () => {
+            console.log("pressed O button");
+            _toggleWindows();
+            gameController.changeSign("O");
+        }, false);
+
+        xBtn.addEventListener("click", () => {
+            _toggleWindows();
+            gameController.changeSign("X");
+        }, false);
+
+        returnBtn.addEventListener("click", () => {
+            _toggleWindows();
+            _clear();
+            gameBoard.clear();
+        }, false);
+
+    })();
+
+    //changes the visuals of the top bar to indicates whose turn it is
+    const changeTurn = () => {
+        console.log("changeTurn");
+        if (playerTurn.classList.contains("active-turn")) {
+            playerTurn.classList.remove("active-turn");
+            computerTurn.classList.add("active-turn");
+        } else if (computerTurn.classList.contains("active-turn")) {
+            playerTurn.classList.add("active-turn");
+            computerTurn.classList.remove("active-turn");
+        }
+    }
+    
+    //removes all X and O from the fields
+    const _clear = () => {
+        _resetRound();
+        _allHtmlFields.forEach(field => {
+            field.textContent = ""; 
+        });
+    }
+
+    //handles visuals at the end of the game 
+    const endGame = (win) => {
+
+        if (win == "Player") {
+            _playerWins++;
+            playerTurn.textContent = `Player: ${_playerWins}`;
+        } else if (win == "Computer") {
+            _computerWins++;
+            computerTurn.textContent = `Computer: ${_computerWins}`;
+        }
+
+        _clear();
+        _changeRound();
+        gameBoard.clear();
+    }
+
+    return {
+        endGame,
+        changeTurn,
+        deactivate,
+        activate
+    }
+
 })();
